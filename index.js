2682aff0bc5e941e913e081670bd578e757979e7
Jose Albornoz <jose.albornoz@outlook.cl>
2024-03-07 07:26:20 -0300

    Ordenamiento de codigo y sacando posibles errores en ruta login y register

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import * as dotenv from "dotenv";
-import cors from "cors";
+import * as dotenv from 'dotenv';
 dotenv.config();
-import express from "express";
-import portalService_route from "./routes/routes.js"
+import cors from 'cors';
+import express from 'express';
+import portalServiceRoutes from './routes/routes.js';
+import { reportarConsulta } from './midlewares/midleware.js';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use("/", portalService_route);
+app.use(reportarConsulta);
+app.use(portalServiceRoutes);
 
-app.use("*", (req, res) => {
-    res.json({ ok: false, result: "404 Pagina no Encontrada" });
+const PORT = process.env.PORT || 3000;
+const URL_BASE = process.env.URL_BASE || 'http://localhost:3000';
+
+app.listen(PORT, () => {
+  console.log('servidor listo en', URL_BASE);
 });
-
-
-const PORT = process.env.PORT || 3002;
-    app.listen(PORT, () => {
-        console.log("servidor listo en http://localhost:" + PORT);
-    });
--- midlewares/midleware.js
+++ midlewares/midleware.js
@@ -2,61 +2,76 @@ import Jwt from 'jsonwebtoken';
 import { handleError } from '../handleError/handleError.js';
 import { comprobarRegistroByEmail } from '../database/consultas.js';
 
-const verificarInput = async (req, res, next) => {
-    try {
-        const {nombre, email, contraseña, ciudad, comuna, direccion, rol} = req.body;
-        console.log("estos son los datos entregados para el registro: ", nombre, email, contraseña, ciudad, comuna, direccion, rol);
-        if(!nombre || !email || !contraseña || !ciudad || !comuna ){
-            throw {
-                code: 400, message: "faltan campos requeridos"
-            }
-        }
-        await comprobarRegistroByEmail(email);
-        console.log("email apto para el registro: ", email);
-        next();
-    } catch (error) {
-        console.log(error);
-        const { status, message } = handleError(error.code);
-        return res.status(status).json({ ok: false, result: message });
-    }
-
-}
-
 const reportarConsulta = async (req, res, next) => {
-    const parametros = req.params;
-    const querys = req.query;
-    const body = req.body;
-    const url = req.url;
-    console.log(`
-    Hoy ${new Date()}
-    Se ha recibido una consulta en la ruta ${url} 
-    con los parámetros, querys y body:
-    `, parametros, querys, body)
-    next(); // informa el codigo y continua al siguiente bloque
-}
+  const parametros = req.params;
+  const querys = req.query;
+  const body = req.body;
+  const url = req.url;
+  console.log(
+    `
+      Hoy ${new Date()}
+      Se ha recibido una consulta en la ruta ${url} 
+      con los parámetros: ${JSON.stringify(parametros)}, 
+      querys: ${JSON.stringify(querys)} y 
+      body: ${JSON.stringify(body)}
+      `
+  );
+
+  next();
+};
+
+const verificarInput = async (req, res, next) => {
+  try {
+    const { nombre, email, contraseña, ciudad, comuna, direccion, rol } =
+      req.body;
+    console.log(
+      'estos son los datos entregados para el registro: ',
+      nombre,
+      email,
+      contraseña,
+      ciudad,
+      comuna,
+      direccion,
+      rol
+    );
+    if (!nombre || !email || !contraseña || !ciudad || !comuna) {
+      throw {
+        code: 400,
+        message: 'faltan campos requeridos',
+      };
+    }
+    const result = await comprobarRegistroByEmail(email);
+    if (result) {
+      throw { code: 400, message: `El email ${email} ya esta registrado.` };
+    }
+    console.log(`El email ${email} es apto para el registro.`);
+    next();
+    
+  } catch (error) {
+    console.log(error);
+    const { status, message } = handleError(error.code, error.message);
+    return res.status(status).json({ ok: false, result: message });
+  }
+};
 
 const verificarCredenciales = async (req, res, next) => {
-    try {
-        const Authorization = req.header("Authorization"); 
-        const token = Authorization.split("Bearer ")[1];
-        console.log(token);
-        if (!token){
-            throw{code: 401, message:"token no proporcionado"}
-        }
-        console.log("verifico el token");
-        Jwt.verify(token, process.env.JWT_PASSWORD ); 
-        const { email } = Jwt.decode(token);
-        req.body.email={ email };
-        next();
-
-    } catch (error) {
-        console.log(error);
-        const { status, message } = handleError(error.code);
-        return res.status(status).json({ ok: false, result: message });
+  try {
+    const Authorization = req.header('Authorization');
+    const token = Authorization.split('Bearer ')[1];
+    console.log(token);
+    if (!token) {
+      throw { code: 401, message: 'token no proporcionado' };
     }
-    
-
-}
-
+    console.log('verifico el token');
+    Jwt.verify(token, process.env.JWT_PASSWORD);
+    const { email } = Jwt.decode(token);
+    req.body.email = { email };
+    next();
+  } catch (error) {
+    console.log(error);
+    const { status, message } = handleError(error.code);
+    return res.status(status).json({ ok: false, result: message });
+  }
+};
 
 export { reportarConsulta, verificarInput, verificarCredenciales };
