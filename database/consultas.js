9cf5629faa5e86794a18d2dc142d1ce0fef72dd2
Jose Albornoz <jose.albornoz@outlook.cl>
2024-03-08 21:55:20 -0300

    Agregando rutas y modificando codigo midleware, controller y consultas

--- database/consultas.js
+++ database/consultas.js
@@ -7,86 +7,74 @@ const pool = new Pool({
   allowExitOnIdle: true,
 });
 
-const registrarUsuario = async (
+const getUser = async (email) => {
+  const consulta = 'SELECT * FROM usuarios WHERE email = $1;';
+  const {
+    rows: [user],
+  } = await pool.query(consulta, [email]);
+  return user;
+};
+
+const checkEmailEnabled = async (email) => {
+  const consulta = 'SELECT email FROM usuarios WHERE email = $1';
+  const { rowCount } = await pool.query(consulta, [email]);
+
+  return rowCount;
+};
+
+const newUser = async (
   nombre,
   email,
   contraseña,
   ciudad,
   comuna,
-  direccion,
-  rol
+  direccion = '',
+  rol = 'user'
 ) => {
-  const values = [nombre, email, contraseña, ciudad, comuna, direccion, rol];
   const consulta =
-    'INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4, $5, $6, $7)';
-  const { rowCount } = await pool.query(consulta, values);
-
-  return rowCount;
-};
-
-//verificar comprobarRegistroByEmail y verificarEmail si cumplen la misma funcion
-
-const comprobarRegistroByEmail = async (email) => {
-  const consulta = 'SELECT * FROM usuarios WHERE email = $1';
-  const { rowCount } = await pool.query(consulta, [email]);
-  
-  return rowCount;
-};
-
-const verificarCredencial = async (email) => {
-  const consulta = 'SELECT * FROM usuarios WHERE email = $1';
-  const values = [email];
+    'INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *;';
+  const values = [nombre, email, contraseña, ciudad, comuna, direccion, rol];
   const {
-    rowCount,
     rows: [user],
   } = await pool.query(consulta, values);
-  console.log('user y rowCount en verificarCredenciales: ', { user, rowCount });
+
   return user;
 };
 
-const obtenerServicios = async ({ limits = 8, page = 1 }) => {
-  const campo = 'publicacion_id';
-  const direccion = 'DESC';
+const getServices = async ({ limits = 8, page = 1 }) => {
+  const order = 'DESC';
+  page = Math.max(1, page);
+  const offset = (page - 1) * limits;
 
-  //const offset = page * limits // iniciar en pagina 0
-  if (page <= 0) {
-    page = 1;
-  }
-  const offset = (page - 1) * limits; // iniciar en pagina 1
-
-  const formattedQuery = format(
-    'SELECT * FROM publicaciones order by %s %s LIMIT %s OFFSET %s',
-    campo,
-    direccion,
+  const publicacionesQuery = format(
+    'SELECT * FROM publicaciones ORDER BY publicacion_id %s LIMIT %s OFFSET %s',
+    order,
     limits,
     offset
   );
+  const { rows: publicaciones } = await pool.query(publicacionesQuery);
 
-  const { rows: publicaciones } = await pool.query(formattedQuery);
-  console.log('se hizo la consulta', publicaciones);
-  return publicaciones;
+  const totalPublicacionesQuery = format('SELECT COUNT(*) FROM publicaciones');
+  const { rows: totalRows } = await pool.query(totalPublicacionesQuery);
+  const totalPublicaciones = parseInt(totalRows[0].count);
+
+  return { publicaciones, totalPublicaciones };
 };
 
-const obtenerPublicaciones = async ({ id, limits = 8, page = 1 }) => {
-  console.log('entro al obtener publicaciones');
-  const campo = 'publicacion_id';
-  const direccion = 'DESC';
-  //const offset = page * limits // iniciar en pagina 0
-  if (page <= 0) {
-    page = 1;
-  }
-  const offset = (page - 1) * limits; // iniciar en pagina 1
-  const formattedQuery = format(
-    'SELECT * FROM publicaciones JOIN usuario ON publicaciones.usuario_id = usuarios.usuario_id WHERE usuarios.usuario_id = $1 order by %s %s LIMIT %s OFFSET %s',
-    id,
-    campo,
-    direccion,
-    limits,
-    offset
-  );
-  const { rows: publicaciones } = await pool.query(formattedQuery);
-  console.log('se hizo la consulta', publicaciones);
-  return publicaciones;
+const getServicesByUser = async ({ userId, limit = 8, page = 1 }) => {
+  const order = 'DESC';
+  page = Math.max(1, page);
+  const offset = (page - 1) * limit;
+  const query = 'SELECT * FROM publicaciones WHERE usuario_id = %s ORDER BY publicacion_id %s LIMIT %s OFFSET %s';
+
+  const publicacionesQuery = format(query, userId, order, limit, offset);
+  const { rows: publicaciones } = await pool.query(publicacionesQuery);
+
+  const totalPublicacionesQuery = format('SELECT COUNT(*) FROM publicaciones WHERE usuario_id = %s', userId);
+  const { rows: totalRows } = await pool.query(totalPublicacionesQuery);
+  const totalPublicaciones = parseInt(totalRows[0].count);
+
+  return { publicaciones, totalPublicaciones };
 };
 
 const agregar_publicacion = async (
@@ -122,28 +110,15 @@ const agregar_publicacion = async (
   return rowCount;
 };
 
-const todas_las_publicaciones = async () => {
-  // toda la tabla
-  const query = 'SELECT * FROM publicaciones';
-  //SELECT * FROM publicaciones WHERE usuario_id = usuario_id
-  const { rows: data } = await pool.query(query);
 
-  return data;
-};
-
-const publicaciones_usuario = async (usuario_id) => {
-  const query = 'SELECT * FROM publicaciones WHERE usuario_id = $1';
-  const { rows: data } = await pool.query(query, usuario_id);
-  return data;
+export const portalModel = {
+  getUser,
+  checkEmailEnabled,
+  newUser,
+  getServices,
+  getServicesByUser
 };
 
 export {
-  registrarUsuario,
-  comprobarRegistroByEmail,
-  verificarCredencial,
-  obtenerServicios,
-  todas_las_publicaciones,
-  obtenerPublicaciones,
   agregar_publicacion,
-  publicaciones_usuario,
 };
--- midlewares/midleware.js
+++ midlewares/midleware.js
@@ -1,8 +1,9 @@
 import Jwt from 'jsonwebtoken';
 import { handleError } from '../handleError/handleError.js';
-import { comprobarRegistroByEmail } from '../database/consultas.js';
+import { portalModel } from '../database/consultas.js';
 
-const reportarConsulta = async (req, res, next) => {
+
+export const requestLogger = async (req, res, next) => {
   const parametros = req.params;
   const querys = req.query;
   const body = req.body;
@@ -20,58 +21,48 @@ const reportarConsulta = async (req, res, next) => {
   next();
 };
 
-const verificarInput = async (req, res, next) => {
+export const verifyRegisterUser = async (req, res, next) => {
   try {
-    const { nombre, email, contraseña, ciudad, comuna, direccion, rol } =
-      req.body;
-    console.log(
-      'estos son los datos entregados para el registro: ',
-      nombre,
-      email,
-      contraseña,
-      ciudad,
-      comuna,
-      direccion,
-      rol
-    );
+    const { nombre, email, contraseña, ciudad, comuna } = req.body;
+
     if (!nombre || !email || !contraseña || !ciudad || !comuna) {
-      throw {
-        code: 400,
-        message: 'faltan campos requeridos',
-      };
+      throw { code: 400, message: 'Faltan campos requeridos.' };
     }
-    const result = await comprobarRegistroByEmail(email);
+
+    const result = await portalModel.checkEmailEnabled(email);
     if (result) {
       throw { code: 400, message: `El email ${email} ya esta registrado.` };
     }
-    console.log(`El email ${email} es apto para el registro.`);
-    next();
     
-  } catch (error) {
-    console.log(error);
-    const { status, message } = handleError(error.code, error.message);
-    return res.status(status).json({ ok: false, result: message });
-  }
-};
-
-const verificarCredenciales = async (req, res, next) => {
-  try {
-    const Authorization = req.header('Authorization');
-    const token = Authorization.split('Bearer ')[1];
-    console.log(token);
-    if (!token) {
-      throw { code: 401, message: 'token no proporcionado' };
-    }
-    console.log('verifico el token');
-    Jwt.verify(token, process.env.JWT_PASSWORD);
-    const { email } = Jwt.decode(token);
-    req.body.email = { email };
     next();
   } catch (error) {
-    console.log(error);
-    const { status, message } = handleError(error.code);
-    return res.status(status).json({ ok: false, result: message });
+    const { status, message } = handleError(error.code, error.message);
+    res.status(status).json({ ok: false, message });
   }
 };
 
-export { reportarConsulta, verificarInput, verificarCredenciales };
+export const verifyCredentials = async (req, res, next) => {
+  try {
+    const authorizationHeader = req.header('Authorization');
+    if (!authorizationHeader) {
+      throw { code: 401, message: 'Token no proporcionado.' };
+    }
+
+    const token = authorizationHeader.split('Bearer ')[1];
+    if (!token) {
+      throw { code: 401, message: 'Formato de token no válido.' };
+    }
+
+    const payload = Jwt.verify(token, process.env.JWT_PASSWORD);
+    if (!payload) {
+      throw { code: 401, message: 'Token invalido.' };
+    }
+
+    req.body.email = payload.email;
+
+    next();
+  } catch (error) {
+    const { status, message } = handleError(error.code, error.message);
+    res.status(status).json({ ok: false, message });
+  }
+};
