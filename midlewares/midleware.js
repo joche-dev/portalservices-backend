426bd879fcb9b439bf75b796af40581043e8d4d9
Jose Albornoz <jose.albornoz@outlook.cl>
2024-03-12 12:34:18 -0300

    Se modifica configuración de CORS

--- midlewares/midleware.js
+++ midlewares/midleware.js
@@ -68,9 +68,9 @@ export const verifyCredentials = async (req, res, next) => {
 };
 
 export const configCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   next();
